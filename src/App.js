644fbd700bdff0a5cd6d34977c8f75e40da56c2c
marcus <marcus.rodrigues10@hotmail.com>
2021-07-30 14:59:27 -0300

    exercise 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route path="/">
+          <Home />
+        </Route>
+      </BrowserRouter>
     );
   }
 }
