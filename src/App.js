e25b47d6f3a218681db31aa42ca4c68d185d9254
marcus <marcus.rodrigues10@hotmail.com>
2021-07-30 14:53:32 -0300

    starting exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Home />
     );
   }
 }
-
-export default App;
