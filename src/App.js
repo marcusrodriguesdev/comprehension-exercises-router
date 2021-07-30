84c593e7056a72f6e6e4877a0f69567a884c5474
marcus <marcus.rodrigues10@hotmail.com>
2021-07-30 16:06:53 -0300

    exercise 9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About'
 import Users from './components/Users';
@@ -9,11 +9,13 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage="Good morning" />  } />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about" component={ About } />
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage="Good morning" />  } />
+        </Switch>
         <ul>
           <Link className="link" to="/">Home</Link>
           <Link className="link" to="/about">About</Link>
